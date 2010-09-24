db4f6ea02bbdfe0a06fe571e805b64da465b52aa
Jon Frisby <jfrisby@mrjoy.com>
2010-09-24 02:06:09 -0500

    DRY.

--- GitStyles.cs
+++ GitStyles.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 using UnityEditor;
 
 public static class GitStyles {
+  private static RectOffset ZeroOffset = new RectOffset(0,0,0,0);
+
   private static GUIStyle _BoldLabel = null;
   public static GUIStyle BoldLabel {
     get {
       if(_BoldLabel == null) {
         _BoldLabel = new GUIStyle(EditorStyles.boldLabel) {
-          padding = new RectOffset(0,0,0,0),
-          margin = new RectOffset(0,0,0,0)
+          padding = ZeroOffset,
+          margin = ZeroOffset
         };
       }
       return _BoldLabel;
@@ -20,8 +22,8 @@ public static class GitStyles {
     get {
       if(_MiniLabel == null) {
         _MiniLabel = new GUIStyle(EditorStyles.miniLabel) {
-          padding = new RectOffset(0,0,0,0),
-          margin = new RectOffset(0,0,0,0)
+          padding = ZeroOffset,
+          margin = ZeroOffset
         };
       }
       return _MiniLabel;
