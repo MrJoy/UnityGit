ac4f7c1e92218ab4580bc6e1e78954359cba3ff2
Jon Frisby <jfrisby@mrjoy.com>
2010-09-24 03:29:17 -0500

    Wrapper API for git.

---
+++ GitWrapper.cs
@@ -0,0 +1,39 @@
+public static class GitWrapper {
+  private static bool _isWorking = true;
+  public static bool IsWorking { get { return _isWorking; } }
+
+  public static string CurrentBranch {
+    get {
+      try {
+        return ShellHelpers.OutputFromCommand("git", "symbolic-ref --quiet HEAD")
+          .Replace("refs/heads/", "").Replace("\n", "");
+      } catch {
+        _isWorking = false;
+        return null;
+      }
+    }
+  }
+
+  public static string Version {
+    get {
+      try {
+        return ShellHelpers.OutputFromCommand("git", "--version").Replace("git version ", "").Replace("\n", "");
+      } catch {
+        _isWorking = false;
+        return null;
+      }
+    }
+  }
+
+  public static string GitBinary {
+    get {
+      // TODO: This is OSX-specific.  We should fix that.
+      try {
+        return ShellHelpers.OutputFromCommand("which", "git").Replace("\n", "");
+      } catch {
+        _isWorking = false;
+        return null;
+      }
+    }
+  }
+}
