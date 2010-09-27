40e47f2a4e97cfa1bd4364e56baa7993775bf02b
Jon Frisby <jfrisby@mrjoy.com>
2010-09-26 19:18:20 -0500

    Tidy things up, don't create GUIContents needlessly.

--- Panels/GitStatusPanel.cs
+++ Panels/GitStatusPanel.cs
@@ -1,27 +1,35 @@
 using UnityEngine;
 using UnityEditor;
+using System.Collections;
 
 public class GitStatusPanel : GitPanel {
-  private GUIContent currentBranchLabel = null;
+  // Messages...
+  private static GUIContent UNSTAGED_CHANGES_LABEL = new GUIContent("Unstaged Changes:"),
+                            STAGED_CHANGES_LABEL = new GUIContent("Staged Changes:"),
+                            TMP_DUMMY_DIFF = new GUIContent("Lorem ipsum dolor sit amar blah blah blah blah blah, blah blah blah.\nLorem ipsum dolor sit amar blah blah blah blah blah, blah blah blah.\nLorem ipsum dolor sit amar blah blah blah blah blah, blah blah blah.\nLorem ipsum dolor sit amar blah blah blah blah blah, blah blah blah.\n"),
+                            CURRENT_BRANCH_LABEL = new GUIContent("Current Branch:"),
+                            REFRESH_BUTTON = new GUIContent("Refresh"),
+                            STAGE_CHANGES_BUTTON = new GUIContent("Stage Changed"),
+                            SIGN_OFF_BUTTON = new GUIContent("Sign Off"),
+                            COMMIT_BUTTON = new GUIContent("Commit"),
+                            PUSH_BUTTON = new GUIContent("Push"),
+                            AMEND_TOGGLE = new GUIContent("Amend");
+
+
+  // Static helper data.
+  private static GUIContent DUMMY_CONTENT = new GUIContent("X");
+  private const int ICON_WIDTH = 16, ITEM_HEIGHT = 21;
+  private static Texture2D BLANK_TEX = null;
+
+
+  // Overarching state for the panel.
+  private GUIContent currentBranchLabel = new GUIContent("");
   private string currentBranch = null;
-
   private bool isDetachedHeadMode = false;
-
   private GitWrapper.Change[] changes = null;
 
-  public void Refresh() {
-    currentBranch = GitWrapper.CurrentBranch;
-    if(currentBranch == "") {
-      currentBranchLabel = new GUIContent("Detached HEAD Mode");
-      isDetachedHeadMode = true;
-    } else {
-      currentBranchLabel = new GUIContent(currentBranch);
-      isDetachedHeadMode = false;
-    }
 
-    changes = GitWrapper.Status;
-  }
-
+  // Event handlers.
   public override void OnEnable() {
     Refresh();
     if(BLANK_TEX == null) {
@@ -35,10 +43,39 @@ public class GitStatusPanel : GitPanel {
     }
   }
 
-  private Vector2 workingScrollPos;
-  private const int ICON_WIDTH = 16, ITEM_HEIGHT = 21;
-  private static Texture2D BLANK_TEX = null;
+  protected void Init() {
+    // Set up data we can't set up until OnGUI.
+    if(editorLineHeight <= 0) {
+      editorLineHeight = GUI.skin.GetStyle("textarea").CalcHeight(DUMMY_CONTENT, 100);
+      boldLabelSpaceSize = SizeOfSpace(GitStyles.BoldLabel);
+    }
+  }
 
+
+  // Operations.
+  public void Refresh() {
+    currentBranch = GitWrapper.CurrentBranch;
+    // TODO: Refactor detection of detached-head state into GitWrapper.
+    if(currentBranch == "") {
+      currentBranchLabel.text = "Detached HEAD Mode";
+      isDetachedHeadMode = true;
+    } else {
+      currentBranchLabel.text = currentBranch;
+      isDetachedHeadMode = false;
+    }
+
+    changes = GitWrapper.Status;
+  }
+
+  public void SignOff() {
+    // TODO: Refactor signoff operation into GitWrapper.
+    string signOffMessage = "Signed-off-by: " + GitWrapper.ConfigGet("user.name") + " <" + GitWrapper.ConfigGet("user.email") + ">";
+    if(!commitMessage.EndsWith(signOffMessage))
+      commitMessage += "\n" + signOffMessage;
+  }
+
+
+  // Helpers.
   private Color ColorForChangeType(GitWrapper.ChangeType status) {
     Color c = Color.red;
     switch(status) {
@@ -55,13 +92,19 @@ public class GitStatusPanel : GitPanel {
     return c;
   }
 
+  private Hashtable iconCache = new Hashtable();
   private void ShowFile(string path, GitWrapper.ChangeType status, bool isSelected) {
     GUIStyle style = isSelected ? GitStyles.FileLabelSelected : GitStyles.FileLabel;
     GUILayout.BeginHorizontal();
-      GUIContent tmp = new GUIContent() {
-        image = AssetDatabase.GetCachedIcon(path) ?? BLANK_TEX,
-        text = null
-      };
+      GUIContent tmp = null;
+      if(!iconCache.ContainsKey(path)) {
+        tmp = new GUIContent() {
+          image = AssetDatabase.GetCachedIcon(path) ?? BLANK_TEX,
+          text = null
+        };
+        iconCache[path] = tmp;
+      }
+      tmp = (GUIContent)iconCache[path];
       GUILayout.Label(tmp, style, GUILayout.Width(ICON_WIDTH), GUILayout.Height(ITEM_HEIGHT));
       Color c = GUI.contentColor;
       GUI.contentColor = ColorForChangeType(status);
@@ -74,8 +117,11 @@ public class GitStatusPanel : GitPanel {
     GUILayout.EndHorizontal();
   }
 
+
+  // Sub-views.
+  private Vector2 workingScrollPos;
   private void ShowUnstagedChanges() {
-    GUILayout.Label("Unstaged Changes:", GitStyles.BoldLabel, NoExpandWidth);
+    GUILayout.Label(UNSTAGED_CHANGES_LABEL, GitStyles.BoldLabel, NoExpandWidth);
     workingScrollPos = EditorGUILayout.BeginScrollView(workingScrollPos, GitStyles.FileListBox);
       if(changes != null) {
         for(int i = 0; i < changes.Length; i++) {
@@ -89,7 +135,7 @@ public class GitStatusPanel : GitPanel {
 
   private Vector2 indexScrollPos;
   private void ShowStagedChanges() {
-    GUILayout.Label("Staged Changes (Will Commit):", GitStyles.BoldLabel, NoExpandWidth);
+    GUILayout.Label(STAGED_CHANGES_LABEL, GitStyles.BoldLabel, NoExpandWidth);
     indexScrollPos = EditorGUILayout.BeginScrollView(indexScrollPos, GitStyles.FileListBox);
       if(changes != null) {
         for(int i = 0; i < changes.Length; i++) {
@@ -104,50 +150,52 @@ public class GitStatusPanel : GitPanel {
   private float editorLineHeight = -1, boldLabelSpaceSize = -1;
   private string commitMessage = "";
   private bool amendCommit = false;
+  public void ShowCommitMessageEditor() {
+    // TODO: Make this scrollable, and make it obey editor commands properly.
+    commitMessage = GUILayout.TextArea(commitMessage, GUILayout.Height(editorLineHeight * 9 + 2));
+    GUILayout.BeginHorizontal();
+      if(GUILayout.Button(REFRESH_BUTTON, GitStyles.CommandLeft)) {
+        Refresh();
+      }
+      GUILayout.Button(STAGE_CHANGES_BUTTON, GitStyles.CommandMid);
+      if(GUILayout.Button(SIGN_OFF_BUTTON, GitStyles.CommandMid))
+        SignOff();
+      GUILayout.Button(COMMIT_BUTTON, GitStyles.CommandMid);
+      GUILayout.Button(PUSH_BUTTON, GitStyles.CommandRight);
+      GUILayout.Space(10);
+      amendCommit = GUILayout.Toggle(amendCommit, AMEND_TOGGLE);
+      GUILayout.FlexibleSpace();
+    GUILayout.EndHorizontal();
+  }
+
+  public void ShowDiffView() {
+    GUILayout.Box(TMP_DUMMY_DIFF, GitStyles.FileListBox, ExpandWidth, ExpandHeight);
+  }
+
   public override void OnGUI() {
-    if(editorLineHeight <= 0) {
-      editorLineHeight = GUI.skin.GetStyle("textarea").CalcHeight(new GUIContent("X"), 100);
-      boldLabelSpaceSize = SizeOfSpace(GitStyles.BoldLabel);
-    }
+    Init();
 
     Color c = GUI.color;
     GUILayout.BeginHorizontal();
       GUILayout.BeginVertical();
         GUILayout.BeginHorizontal();
-          GUILayout.Label("Current Branch:", GitStyles.BoldLabel, NoExpandWidth);
+          GUILayout.Label(CURRENT_BRANCH_LABEL, GitStyles.BoldLabel, NoExpandWidth);
           GUILayout.Space(boldLabelSpaceSize);
           GUI.color = isDetachedHeadMode ? GitStyles.ErrorColor : GitStyles.TextColor;
           GUILayout.Label(currentBranchLabel, GitStyles.WhiteBoldLabel, NoExpandWidth);
           GUI.color = c;
         GUILayout.EndHorizontal();
-        EditorGUILayout.Separator();
+        GUILayout.Space(5);
 
         ShowUnstagedChanges();
-        EditorGUILayout.Separator();
+        GUILayout.Space(5);
 
         ShowStagedChanges();
       GUILayout.EndVertical();
       GUILayout.Space(5);
       GUILayout.BeginVertical();
-        GUILayout.Box("Lorem ipsum dolor sit amar blah blah blah blah blah, blah blah blah.\nLorem ipsum dolor sit amar blah blah blah blah blah, blah blah blah.\nLorem ipsum dolor sit amar blah blah blah blah blah, blah blah blah.\nLorem ipsum dolor sit amar blah blah blah blah blah, blah blah blah.\n", GitStyles.FileListBox, ExpandWidth, ExpandHeight);
-        // TODO: Make this scrollable, and make it obey editor commands properly.
-        commitMessage = GUILayout.TextArea(commitMessage, GUILayout.Height(editorLineHeight * 9 + 2));
-        GUILayout.BeginHorizontal();
-          if(GUILayout.Button("Refresh", GitStyles.CommandLeft)) {
-            Refresh();
-          }
-          GUILayout.Button("Stage Changed", GitStyles.CommandMid);
-          if(GUILayout.Button("Sign Off", GitStyles.CommandMid)) {
-            string signOffMessage = "Signed-off-by: " + GitWrapper.ConfigGet("user.name") + " <" + GitWrapper.ConfigGet("user.email") + ">";
-            if(!commitMessage.EndsWith(signOffMessage)) {
-              commitMessage += "\n" + signOffMessage;
-            }
-          }
-          GUILayout.Button("Commit", GitStyles.CommandMid);
-          GUILayout.Button("Push", GitStyles.CommandRight);
-          GUILayout.Space(10);
-          amendCommit = GUILayout.Toggle(amendCommit, "Amend");
-        GUILayout.EndHorizontal();
+        ShowDiffView();
+        ShowCommitMessageEditor();
       GUILayout.EndVertical();
     GUILayout.EndHorizontal();
   }
